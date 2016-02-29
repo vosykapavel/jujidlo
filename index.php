965fadd3ca92bbdfdb9cdaa3e8ebfa5d038fe295
Pavel Vosyka <vosykapavel@gmail.com>
2016-02-29 17:18:11 +0100

    Studentska + Minutkova together

--- index.php
+++ index.php
@@ -16,12 +16,18 @@ class Dny{
       return $this->dny;
   }
   public function getDen($datum){
-    foreach ($dny as $key => $den) {
-      if($den->datum == $datum){
-        return $den;
+    if(!empty($this->dny)){
+//      var_dump($this->dny[0]);
+      foreach ($this->dny as $key => $den) {//$key =>
+        if($this->dny[$key]->getDatum() == $datum){
+          return $den;
+          break;
+        }
       }
     }
+    return false;
   }
+
 }
 
 class Den{
@@ -96,58 +102,57 @@ function validateDate($date)
     $d = DateTime::createFromFormat('j.n.Y', $date);
     return $d && $d->format('j.n.Y') == $date;
 }
-$jidelna = new Jidelna();
-$studentska = file_get_contents("http://menza.jcu.cz/Studentska.html");
 //$studentska = file_get_contents("jidelnicky/15-10-29.html");
-$studentska = str_replace('&nbsp;', '', iconv('WINDOWS-1250', 'UTF-8', $studentska));
 
-$doc = phpQuery::newDocumentHTML($studentska);
-//echo $studentska;
-$rows = $doc["table  tr"];
-//echo $rows;
-//echo "$rows[0] ahoj \n";
-/*
-foreach ($doc["table tr"] as $tr) {
-//    echo pq($tr)->html()." \n ";
-//    $tr["td:first"]->text();
-    if(validateDate(pq()->text())){
-      echo $tr["td:first"]->html();
+function parse($url = "http://menza.jcu.cz/Studentska.html"){
+    $jidelnicek = file_get_contents($url);
+    $jidelnicek = str_replace('&nbsp;', '', iconv('WINDOWS-1250', 'UTF-8', $jidelnicek));
+    $doc = phpQuery::newDocumentHTML($jidelnicek);
+    $rows = $doc["table  tr"];
+    $radekDne = 0;
+    $datumDne = "";
+    $den = NULL;
+    //echo $rows;
+    foreach(pq($rows) as $row)
+    {
+      global $jidelna, $dny;
+
+
+        $tr = pq($row);
+        $datum = $tr['td:nth-child(1)']->text();
+
+        // tady začínají jídla dne - datumem
+        if(validateDate($datum)){
+          $radekDne = 1;
+          $datumDne = $datum;
+          $uzExistuje = $dny->getDen($datum);
+          if(!$uzExistuje){
+            $den = new Den($datum);
+            $dny->ulozit($den);
+          }else{
+            $den = $uzExistuje;
+          }
+
+        }elseif($radekDne>0){
+          $radekDne++;
+        }
+        if($radekDne>0){
+          $typJidla = $tr['td:nth-child(2)']->text();
+          $alergeny = explode(', ',$tr['td:nth-child(3)']->text());
+          $nazev = $tr['td:nth-child(4)']->text();
+          if($nazev != ""){
+            $j = new Jidlo($datumDne, $typJidla, $alergeny, $nazev);
+            $den->addJidlo($j);
+          }
+    //      var_dump($jidelna->getJidla());
+        }
+
+    //    $prvni =pq($row)->text();
     }
-}
-*/
-$radekDne = 0;
-$datumDne = "";
-$den = NULL;
-$dny = new Dny();
-//echo $rows;
-foreach(pq($rows) as $row)
-{
-
-    $tr = pq($row);
-    $datum = $tr['td:nth-child(1)']->text();
-
-    // tady začínají jídla dne - datumem
-    if(validateDate($datum)){
-      $radekDne = 1;
-      $datumDne = $datum;
-      $den = new Den($datum);
-      $dny->ulozit($den);
-    }elseif($radekDne>0){
-      $radekDne++;
-    }
-    if($radekDne>0){
-      $typJidla = $tr['td:nth-child(2)']->text();
-      $alergeny = explode(', ',$tr['td:nth-child(3)']->text());
-      $nazev = $tr['td:nth-child(4)']->text();
-      if($nazev != ""){
-        $j = new Jidlo($datumDne, $typJidla, $alergeny, $nazev);
-        $den->addJidlo($j);
-      }
-//      var_dump($jidelna->getJidla());
-    }
-
-//    $prvni =pq($row)->text();
-}
+  }
 //      echo ucfirst(strftime('%A',DateTime::createFromFormat('j.n.Y', $tdDatum)->format('U')));
-
+$jidelna = new Jidelna();
+$dny = new Dny();
+parse();
+parse("http://menza.jcu.cz/Minutkova.html");
 include "template.php";
