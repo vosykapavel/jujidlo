51a32607a23a81e75e8897a70c72dbef3075e82b
Pavel Vosyka <vosykapavel@gmail.com>
2016-05-16 14:15:59 +0200

    Reklama damejidlo.cz

--- footer.php
+++ footer.php
@@ -1,1 +1,5 @@
+<h2>Nevybrali jste si?</h2>
+<a href="http://www.dpbolvw.net/click-8071614-11422658-1447927030000" target="_top" class="reklama">
+<img src="http://www.awltovhc.com/image-8071614-11422658-1447927030000" width="728" height="90" alt="" border="0"/></a>
+<br>
 <small>Tento jídelníček bude postupně chytřejší a hlavně snadno použitelný. Komentáře posílejte na vosykapavel@gmail.com.</small>
